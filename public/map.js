baa3f89ba97b009297a4723fad246162dc4469fa
S-Traut <stephane.traut@gmail.com>
2020-08-10 00:31:56 +0200

    add marker // modified header

--- public/map.js
+++ public/map.js
@@ -1,11 +1,25 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+//Map initialization
 
-L.tileLayer('https://api.mapbox.com/styles/v1/ponoyoshi/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'ckdm2fu982ojs1inz2yisetpn',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoicG9ub3lvc2hpIiwiYSI6ImNrZG0xdHR1azE0Nnkyem1yaXh6NWYycmMifQ.zN5LQ3gdujetU6WDIT3CMQ'
-}).addTo(mymap);
 
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
+
+function showPosition(position) {
+    //x.innerHTML = "Latitude: " + position.coords.latitude +
+    //    "<br>Longitude: " + position.coords.longitude;
+    var mymap = L.map('mapid').setView([position.coords.latitude, position.coords.longitude], 13);
+    var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap);
+    marker.bindPopup("Your actual position!").openPopup();
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/ponoyoshi/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'ckdm2fu982ojs1inz2yisetpn',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoicG9ub3lvc2hpIiwiYSI6ImNrZG0xdHR1azE0Nnkyem1yaXh6NWYycmMifQ.zN5LQ3gdujetU6WDIT3CMQ'
+    }).addTo(mymap);
+}
